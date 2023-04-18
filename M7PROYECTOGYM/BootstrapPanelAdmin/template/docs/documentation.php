43cd68b3377808329966c21a68598e33c37f337c
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2023-04-18 12:55:20 +0200

    Add files via upload

---
+++ M7PROYECTOGYM/BootstrapPanelAdmin/template/docs/documentation.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Kapella Admin Dashboard</title>
+  <!-- base:css -->
+  <link rel="stylesheet" href="../vendors/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../css/style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="../images/favicon.png" />
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="main-panel w-100  documentation">
+        <div class="content-wrapper">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-12 doc-header">
+                <a class="btn btn-success" href="../../../pantalla_admin.php"><i class="mdi mdi-home me-2"></i>Volver al Menu</a>
+                <h1 class="text-primary mt-4">Información</h1>
+              </div>
+            </div>
+            <div class="row doc-content">
+              <div class="col-12 col-md-10 offset-md-1">
+                <div class="col-12 grid-margin" id="doc-intro">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="mb-4 mt-4">Introducción</h3>
+                            <p>¡Bienvenido a Azure! Somos una empresa líder en la industria del fitness, dedicada a ayudarte a alcanzar tus objetivos de bienestar y salud. Nuestro gimnasio ofrece una amplia gama de instalaciones y servicios de última generación, diseñados para brindarte una experiencia de entrenamiento excepcional.</p>
+                            <p>Desde nuestros modernos equipos de ejercicio hasta nuestras clases de fitness de primer nivel y nuestro equipo altamente capacitado, nos enorgullecemos de brindarte un entorno motivador y acogedor para que puedas alcanzar tu máximo potencial. Ya sea que estés buscando mejorar tu estado físico, aumentar tu fuerza, perder peso o simplemente mantener un estilo de vida activo y saludable, en Azure encontrarás todo lo que necesitas para alcanzar tus metas. </p>
+                            <p>Únete a nosotros y descubre cómo podemos ayudarte a vivir una vida más activa y saludable en un ambiente de primera clase. ¡Te esperamos en Azure!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 grid-margin" id="doc-started">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="mb-4">Azure APP</h3>
+                            <p>Azure Fitness es una empresa líder en la industria del fitness con una trayectoria de 4 años de éxito y crecimiento. Desde su fundación, hemos estado comprometidos en brindar una experiencia excepcional a nuestros miembros, ayudándolos a alcanzar sus metas de bienestar y salud. Nuestro gimnasio cuenta con instalaciones de vanguardia, equipadas con los últimos equipos de ejercicio de alta tecnología, diseñados para satisfacer las necesidades de nuestros miembros, desde principiantes hasta atletas experimentados. Ofrecemos una amplia gama de clases de fitness, desde entrenamiento funcional, yoga, pilates, hasta clases de cardio y fuerza, impartidas por un equipo de entrenadores altamente capacitados y comprometidos con el éxito de nuestros miembros. </p>
+                            <p>En Azure Fitness, valoramos la comodidad y satisfacción de nuestros miembros, por lo que ofrecemos un ambiente limpio, moderno y acogedor, con un servicio al cliente excepcional. Nos preocupamos por la salud y bienestar de nuestros miembros, por lo que también contamos con programas de nutrición y asesoría personalizada para ayudarlos a alcanzar un enfoque integral de la salud y el fitness. Nuestra empresa se enorgullece de ser una parte activa de la comunidad, participando en eventos locales, promoviendo un estilo de vida activo y saludable, y apoyando causas benéficas en nuestra área. </p>
+                            
+                            <p>En resumen, con 4 años de experiencia en la industria del fitness, Azure Fitness se ha posicionado como una empresa líder que ofrece una experiencia de entrenamiento de primer nivel, con instalaciones de vanguardia, un equipo altamente capacitado y un enfoque integral en la salud y bienestar de nuestros miembros. Estamos comprometidos en ayudar a nuestros miembros a alcanzar sus metas y vivir una vida activa y saludable. ¡Te invitamos a unirte a nuestra comunidad en Azure Fitness!</p>
+                            <div class="alert alert-success mt-4 d-flex align-items-center" role="alert">
+                              <i class="ti-info-alt me-4"></i>
+                              <p>¡Regístrate en la aplicación de Azure Fitness y disfruta de una experiencia rápida y sencilla para unirte a nuestro gimnasio y reservar tus actividades favoritas! Gestiona tu membresía y actividades de manera conveniente en nuestra aplicación diseñada para hacerte la vida más fácil. ¡Únete a nuestra comunidad activa y saludable en Azure Fitness hoy mismo!</p>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- partial:../partials/_footer.html -->
+        <footer class="footer">
+          <div class="footer-wrap">
+            <div class="d-sm-flex justify-content-center justify-content-sm-between">
+              <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2021</span>
+              <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="https://www.bootstrapdash.com/" target="_blank"> Bootstrap dashboard </a> templates</span>
+            </div>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
