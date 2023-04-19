b25b75abad73acb8e68aabc92f71bcbb78cf97d0
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2023-04-19 12:46:04 +0200

    Nuevos cambios
    cambios

---
+++ M7PROYECTOGYM/InsBtn.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Inicio de Sesión</title>
+    <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="Bootstrap/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="Bootstrap/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="Bootstrap/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/util.css">
+	<link rel="stylesheet" type="text/css" href="Bootstrap/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+    
+<div class="limiter">
+    <div class="container-login100" style="background-image: url('Bootstrap/images/bg-01.jpg');">
+        <div style="width: 50%;" class="wrap-login100">
+            <form class="login100-form validate-form" method="post" action="login-proceso.php">
+                <span class="login100-form-logo">
+                    <img src="Bootstrap/images/Logoazura.png" alt="Logo de la empresa" height="100">
+                </span>
+
+                <span class="login100-form-title p-b-34 p-t-27">
+                    Inscripción Realizada
+                </span>
+                <div class="container-login100-form-btn">
+                
+
+                    <a href="iniciosesion.php" class="login100-form-btn" style="font-size: 24px; width: 280px; height: 60px;">
+                    Salir
+                    </a>
+                </div>
+
+                
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+    <div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="Bootstrap/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="Bootstrap/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="Bootstrap/vendor/bootstrap/js/popper.js"></script>
+	<script src="Bootstrap/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="Bootstrap/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="Bootstrap/vendor/daterangepicker/moment.min.js"></script>
+	<script src="Bootstrap/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="Bootstrap/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="Bootstrap/js/main.js"></script>
+</body>
+</html>
