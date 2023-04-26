c17e965af99d309ffdac9cc0757809310b1b239a
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2023-04-26 08:11:42 +0200

    Update pantalla_admin.php

--- M7PROYECTOGYM/pantalla_admin.php
+++ M7PROYECTOGYM/pantalla_admin.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Kapella Bootstrap Admin Dashboard Template</title>
+  <title>Panel Admin</title>
   <!-- base:css -->
   <link rel="stylesheet" href="BootstrapPanelAdmin/template/vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="BootstrapPanelAdmin/template/vendors/base/vendor.bundle.base.css">
